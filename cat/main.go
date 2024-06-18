0f3817b7b37ec7c9955606716593481b04af92ab
isseii10 <t1l.g2.o11g@gmail.com>
2024-06-19 02:02:55 +0900

    cat

---
+++ cat/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// $ cat a b c > out
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "%s: file name not given\n", os.Args[0])
+	}
+	for _, v := range os.Args[1:] {
+		doCat(v)
+	}
+}
+
+func doCat(s string) {
+	f, err := os.Open(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open file %s: %v", s, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read file %s: %v", s, err)
+		os.Exit(1)
+	}
+	if _, err := io.WriteString(os.Stdout, string(b)); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write: %v", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/isseii10/go-linux-cmd
+
+go 1.22.4
